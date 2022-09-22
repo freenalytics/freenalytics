a0649381e763b488c5e34a3361e5040671378658
moonstar-x <christian.lopez99@outlook.com>
2022-09-22 12:12:52 -0500

    Extended User model.

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,13 +1,30 @@
 import { Schema, model } from 'mongoose';
 import passportLocalMongoose from 'passport-local-mongoose';
 
+export interface UserJwtPayload {
+  id: string
+  username: string
+  createdAt: string
+  locale: string
+}
+
 export interface UserModel {
   _id: string
   username: string
+  createdAt: string
+  locale: string
 }
 
 const userSchema = new Schema({
   username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: String,
+    required: true
+  },
+  locale: {
     type: String,
     required: true
   }
