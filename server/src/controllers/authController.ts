ca5bbc3635bfc5bcb475c74ad9668a84c6c6197e
moonstar-x <christian.lopez99@outlook.com>
2022-09-22 12:41:22 -0500

    Controllers now use ResponseBuilder.

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,22 +1,28 @@
 import { Request, Response } from 'express';
+import HttpStatus from 'http-status-codes';
 import User, { UserModel } from '../models/user';
 import { createJwtToken } from '../middleware/auth';
+import { ResponseBuilder } from '../utils/http';
 
 export type RegisterBody = {
   username: string,
-  password: string
+  password: string,
+  locale: string
 }
 
 export const register = async (req: Request, res: Response) => {
-  const { username, password } = req.body as RegisterBody;
+  const { username, password, locale } = req.body as RegisterBody;
+  const createdAt = new Date().toISOString();
 
   try {
-    await User.register(new User({ username }), password);
+    await User.register(new User({ username, locale, createdAt }), password);
+    const response = new ResponseBuilder()
+      .withStatusCode(HttpStatus.OK)
+      .withData({
+        message: 'Account creation successful. Please login to get your access token.'
+      });
 
-    // TODO: Use a ResponseBuilder.
-    res.json({
-      message: 'Success'
-    });
+    res.status(response.statusCode).send(response.build());
   } catch (error) {
     // TODO: Rethrow correct error.
     throw error;
@@ -24,9 +30,12 @@ export const register = async (req: Request, res: Response) => {
 };
 
 export const login = async (req: Request, res: Response) => {
-  const { _id: id, username } = req.user as UserModel;
-  const payload = { id, username };
+  const { _id: id, username, locale, createdAt } = req.user as UserModel;
+  const response = new ResponseBuilder()
+    .withStatusCode(HttpStatus.OK)
+    .withData({
+      token: createJwtToken({ id, username, locale, createdAt })
+    });
 
-  // TODO: Use ResponseBuilder.
-  res.json({ token: createJwtToken(payload) });
+  res.status(response.statusCode).send(response.build());
 };
--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
+import HttpStatus from 'http-status-codes';
 import User from '../models/user';
+import { ResponseBuilder } from '../utils/http';
 
 export const getUsers = async (_: Request, res: Response) => {
   const users = await User.find({});
+  const response = new ResponseBuilder()
+    .withStatusCode(HttpStatus.OK)
+    .withData(users);
 
-  // TODO: Use ResponseBuilder.
-  res.json(users);
+  res.status(response.statusCode).send(response.build());
 };
