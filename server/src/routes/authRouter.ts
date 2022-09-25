f5878355925d702d98e1f92e1ce4ffaf80bc198c
moonstar-x <christian.lopez99@outlook.com>
2022-09-24 23:13:51 -0500

    Added onlySupportedMethods to current routers.

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { localAuthenticate } from '../middleware/auth';
+import { onlySupportedMethods } from '../middleware/routes';
 import * as authController from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.all('/register', onlySupportedMethods('POST'));
+
 router.post('/login', localAuthenticate, authController.login);
+router.all('/login', onlySupportedMethods('POST'));
+
 router.get('/registration-open', authController.registrationOpen);
+router.all('/registration-open', onlySupportedMethods('GET'));
 
 export default router;
