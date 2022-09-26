73d4259c1ea325ae39c36fa7dbf01ae775ead1ac
moonstar-x <christian.lopez99@outlook.com>
2022-09-25 21:01:20 -0500

    Eliminated duplicate code in userController for handleGetByUsername.

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -24,9 +24,7 @@ export const getAll = async (_: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export const getByUsername = async (req: Request, res: Response, next: NextFunction) => {
-  const { username } = req.params;
-
+const handleGetByUsername = async (username: string, res: Response, next: NextFunction) => {
   try {
     const user = await getUserByUsername(username);
     const response = new ResponseBuilder()
@@ -43,23 +41,14 @@ export const getByUsername = async (req: Request, res: Response, next: NextFunct
   }
 };
 
+export const getByUsername = async (req: Request, res: Response, next: NextFunction) => {
+  const { username } = req.params;
+  await handleGetByUsername(username, res, next);
+};
+
 export const getCurrent = async (req: Request, res: Response, next: NextFunction) => {
   const { username } = req.user as UserModel;
-
-  try {
-    const user = await getUserByUsername(username);
-    const response = new ResponseBuilder()
-      .withStatusCode(HttpStatus.OK)
-      .withData(user);
-
-    res.status(response.statusCode).send(response.build());
-  } catch (error: any) {
-    if (error instanceof HttpError) {
-      next(error);
-    } else {
-      next(new InternalServerError(error.message));
-    }
-  }
+  await handleGetByUsername(username, res, next);
 };
 
 export const updateCurrent = async (req: Request, res: Response, next: NextFunction) => {
