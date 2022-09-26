a2912e7f0cc8aac4feb7ae4e481a722576aed850
moonstar-x <christian.lopez99@outlook.com>
2022-09-25 20:57:09 -0500

    Created updateCurrent function in userController.

--- server/src/controllers/userController.spec.ts
+++ server/src/controllers/userController.spec.ts
@@ -1,8 +1,8 @@
-import { getAll, getByUsername } from './userController';
+import { getAll, getByUsername, updateCurrent } from './userController';
 import { Request, Response } from 'express';
 import { ResponseMock } from '../../__mocks__/http_mocks';
 import * as userService from '../services/userService';
-import { InternalServerError, ResourceNotFoundError } from '../errors/http';
+import { BadRequestError, InternalServerError, ResourceNotFoundError, SchemaValidationError } from '../errors/http';
 
 const mockedUsers = [
   { username: 'moon' },
@@ -84,6 +84,74 @@ describe('Controllers: UserController', () => {
     it('should call next with an InternalServerError if an error occurs.', async () => {
       getUserByUsernameSpy.mockRejectedValueOnce({ message: 'Oops' });
       await getByUsername(req, res, nextMock);
+
+      expect(nextMock).toHaveBeenCalledTimes(1);
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(InternalServerError);
+    });
+  });
+
+  describe('updateCurrent()', () => {
+    const req = {
+      user: {
+        username: 'moon'
+      },
+      body: {
+        locale: 'en'
+      }
+    } as unknown as Request;
+
+    const updateUserByUsernameSpy = jest.spyOn(userService as any, 'updateUserByUsername')
+      .mockResolvedValue([mockedUsers]);
+
+    beforeEach(() => {
+      updateUserByUsernameSpy.mockClear();
+    });
+
+    it('should call next with a BadRequestError if no body is provided.', async () => {
+      const req = {
+        user: {
+          username: 'moon'
+        }
+      } as unknown as Request;
+      await updateCurrent(req, res, nextMock);
+
+      expect(nextMock).toHaveBeenCalledTimes(1);
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(BadRequestError);
+    });
+
+    it('should call next with a SchemaValidationError if the body provided is invalid.', async () => {
+      const req = {
+        user: {
+          username: 'moon'
+        },
+        body: {
+          illegal: 'yes'
+        }
+      } as unknown as Request;
+      await updateCurrent(req, res, nextMock);
+
+      expect(nextMock).toHaveBeenCalledTimes(1);
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(SchemaValidationError);
+    });
+
+    it('should respond with the updated user.', async () => {
+      await updateCurrent(req, res, nextMock);
+
+      expect(res.send).toHaveBeenCalledTimes(1);
+      expect((res.send as jest.Mock).mock.calls[0][0].data).toMatchObject([mockedUsers]);
+    });
+
+    it('should call next with the error if an HttpError occurs.', async () => {
+      updateUserByUsernameSpy.mockRejectedValueOnce(new ResourceNotFoundError(''));
+      await updateCurrent(req, res, nextMock);
+
+      expect(nextMock).toHaveBeenCalledTimes(1);
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(ResourceNotFoundError);
+    });
+
+    it('should call next with an InternalServerError if an error occurs.', async () => {
+      updateUserByUsernameSpy.mockRejectedValueOnce({ message: 'Oops' });
+      await updateCurrent(req, res, nextMock);
 
       expect(nextMock).toHaveBeenCalledTimes(1);
       expect(nextMock.mock.calls[0][0]).toBeInstanceOf(InternalServerError);
--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpStatus from 'http-status-codes';
-import { getAllUsers, getUserByUsername } from '../services/userService';
+import { getAllUsers, getUserByUsername, updateUserByUsername } from '../services/userService';
 import { ResponseBuilder } from '../utils/http';
-import { HttpError, InternalServerError } from '../errors/http';
+import { BadRequestError, HttpError, InternalServerError } from '../errors/http';
+import { UserUpdateBody, UserUpdateSchema } from '../schemas/user';
+import { validate } from '../utils/schema';
+import { UserModel } from '../models/user';
 
 export const getAll = async (_: Request, res: Response, next: NextFunction) => {
   try {
@@ -29,6 +32,33 @@ export const getByUsername = async (req: Request, res: Response, next: NextFunct
     const response = new ResponseBuilder()
       .withStatusCode(HttpStatus.OK)
       .withData(user);
+
+    res.status(response.statusCode).send(response.build());
+  } catch (error: any) {
+    if (error instanceof HttpError) {
+      next(error);
+    } else {
+      next(new InternalServerError(error.message));
+    }
+  }
+};
+
+export const updateCurrent = async (req: Request, res: Response, next: NextFunction) => {
+  const { username } = req.user as UserModel;
+  const updateBody = req.body as UserUpdateBody;
+
+  if (!updateBody) {
+    next(new BadRequestError('A JSON body is required.'));
+    return;
+  }
+
+  try {
+    const updatedFields = validate(updateBody, UserUpdateSchema);
+    const updatedUser = await updateUserByUsername(username, updatedFields);
+
+    const response = new ResponseBuilder()
+      .withStatusCode(HttpStatus.OK)
+      .withData(updatedUser);
 
     res.status(response.statusCode).send(response.build());
   } catch (error: any) {
