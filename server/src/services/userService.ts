bc369b7982b6c1e1c8aee05a9969e6e484102a96
moonstar-x <christian.lopez99@outlook.com>
2022-09-25 20:37:32 -0500

    Created updateUserByUsername in userService.

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,5 +1,6 @@
 import User, { UserModel } from '../models/user';
 import { ResourceNotFoundError } from '../errors/http';
+import { UserUpdateBody } from '../schemas/user';
 
 export const getAllUsers = (): Promise<UserModel[]> => {
   return User.find({}, { _id: 0, __v: 0 }).exec();
@@ -26,3 +27,16 @@ export const getUserByUsername = async (username: string): Promise<UserModel> =>
   return user;
 };
 
+export const updateUserByUsername = async (username: string, updatedFields: UserUpdateBody): Promise<UserModel> => {
+  const updatedUser = await User.findOneAndUpdate({ username }, { $set: updatedFields }, {
+    returnDocument: 'after',
+    upsert: false,
+    projection: { _id: 0, __v: 0 }
+  });
+
+  if (!updatedUser) {
+    throw new ResourceNotFoundError(`User ${username} was not found.`);
+  }
+
+  return updatedUser;
+};
