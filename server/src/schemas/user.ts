ad4837830afca39688286eaf26c953a714b991b0
moonstar-x <christian.lopez99@outlook.com>
2022-09-25 19:08:13 -0500

    Updated schema validation messages to fit field names.

--- server/src/schemas/user.ts
+++ server/src/schemas/user.ts
@@ -12,13 +12,13 @@ export interface UserLoginBody {
 export const UserLoginSchema = Joi.object<UserLoginBody>({
   username: Joi.string().lowercase().trim().required()
     .messages({
-      'string.empty': 'Username is required.',
-      'any.required': 'Username is required.'
+      'string.empty': 'username is required.',
+      'any.required': 'username is required.'
     }),
   password: Joi.string().required()
     .messages({
-      'string.empty': 'Password is required.',
-      'any.required': 'Password is required.'
+      'string.empty': 'password is required.',
+      'any.required': 'password is required.'
     })
 });
 
@@ -38,25 +38,25 @@ export interface UserRegisterBody {
 export const UserRegisterSchema = Joi.object<UserRegisterBody>({
   username: Joi.string().min(USERNAME_MIN_LENGTH).max(USERNAME_MAX_LENGTH).pattern(VALID_USERNAME_REGEX).lowercase().trim().required()
     .messages({
-      'string.min': `Username length must be greater than ${USERNAME_MIN_LENGTH}.`,
-      'string.max': `Username length must be less than ${USERNAME_MAX_LENGTH}.`,
-      'string.empty': 'Username is required.',
-      'any.required': 'Username is required.'
+      'string.min': `username length must be greater than ${USERNAME_MIN_LENGTH}.`,
+      'string.max': `username length must be less than ${USERNAME_MAX_LENGTH}.`,
+      'string.empty': 'username is required.',
+      'any.required': 'username is required.'
     }),
   password: joiPassword.string().min(PASSWORD_MIN_LENGTH).minOfSpecialCharacters(1).minOfLowercase(1).minOfUppercase(1).noWhiteSpaces().required()
     .messages({
-      'password.minOfSpecialCharacters': 'Password must have at least one special character.',
-      'password.minOfLowercase': 'Password must have at least one lowercase letter.',
-      'password.minOfUppercase': 'Password must have at least one uppercase letter.',
-      'password.noWhiteSpaces': 'Password cannot contain spaces.',
-      'string.empty': 'Password is required.',
-      'any.required': 'Password is required.'
+      'password.minOfSpecialCharacters': 'password must have at least one special character.',
+      'password.minOfLowercase': 'password must have at least one lowercase letter.',
+      'password.minOfUppercase': 'password must have at least one uppercase letter.',
+      'password.noWhiteSpaces': 'password cannot contain spaces.',
+      'string.empty': 'password is required.',
+      'any.required': 'password is required.'
     }),
   locale: Joi.string().valid(...VALID_LOCALES).required()
     .messages({
-      'any.only': `Locale must be one of: ${VALID_LOCALES.join(', ')}.`,
-      'string.empty': 'Locale is required.',
-      'any.required': 'Locale is required.'
+      'any.only': `locale must be one of: ${VALID_LOCALES.join(', ')}.`,
+      'string.empty': 'locale is required.',
+      'any.required': 'locale is required.'
     })
 });
 
@@ -68,16 +68,16 @@ export interface UserChangePasswordBody {
 export const UserChangePasswordSchema = Joi.object<UserChangePasswordBody>({
   old_password: Joi.string().required()
     .messages({
-      'string.empty': 'Old password is required.',
-      'any.required': 'Old password is required.'
+      'string.empty': 'old_password is required.',
+      'any.required': 'old_password is required.'
     }),
   new_password: joiPassword.string().min(PASSWORD_MIN_LENGTH).minOfSpecialCharacters(1).minOfLowercase(1).minOfUppercase(1).noWhiteSpaces().required()
     .messages({
-      'password.minOfSpecialCharacters': 'New password must have at least one special character.',
-      'password.minOfLowercase': 'New password must have at least one lowercase letter.',
-      'password.minOfUppercase': 'New password must have at least one uppercase letter.',
-      'password.noWhiteSpaces': 'New password cannot contain spaces.',
-      'string.empty': 'New password is required.',
-      'any.required': 'New password is required.'
+      'password.minOfSpecialCharacters': 'new_password must have at least one special character.',
+      'password.minOfLowercase': 'new_password must have at least one lowercase letter.',
+      'password.minOfUppercase': 'new_password must have at least one uppercase letter.',
+      'password.noWhiteSpaces': 'new_password cannot contain spaces.',
+      'string.empty': 'new_password is required.',
+      'any.required': 'new_password is required.'
     })
 });
