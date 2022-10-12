484d27aff4efdddf3ea79d48cfae788eb69d85c8
moonstar-x <christian.lopez99@outlook.com>
2022-10-12 01:43:23 -0500

    Applications page now fetches applications and renders them.

--- web-dashboard/src/pages/applications/ApplicationsPage.tsx
+++ web-dashboard/src/pages/applications/ApplicationsPage.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Heading } from 'react-bulma-components';
+import { useQuery } from '@tanstack/react-query';
 import PageWrapper from '../../components/common/pageWrapper';
+import Loading from '../../components/common/loading';
+import RequestErrorMessageFullPage from '../../components/common/requestErrorMessageFullPage';
+import ApplicationCardGroup from '../../components/pageComponents/applications/applicationCardGroup';
 import useTitle from '../../hooks/title';
+import useLocale from '../../hooks/locale';
+import useApi from '../../hooks/api';
 
 const ApplicationsPage: React.FC = () => {
   useTitle('pages.applications.title');
+  const { t } = useLocale();
+  const { client } = useApi();
+  const request = client.application.getApplications();
+  const { isLoading, error, data: applications } = useQuery(request.key, request.fn);
+
+  if (isLoading) {
+    return (
+      <Loading />
+    );
+  }
+
+  if (error) {
+    return (
+      <RequestErrorMessageFullPage error={error as Error} />
+    );
+  }
 
   return (
     <PageWrapper className="applications-page">
-      Applications
+      <Heading>
+        {t('pages.applications.title')}
+      </Heading>
+      <ApplicationCardGroup applications={applications!} />
     </PageWrapper>
   );
 };
