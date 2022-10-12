f7c4947c824d47780e27c577140d35977b576485
moonstar-x <christian.lopez99@outlook.com>
2022-10-12 03:37:05 -0500

    Added new icon.

--- web-dashboard/src/utils/icons.ts
+++ web-dashboard/src/utils/icons.ts
@@ -1,6 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faServer } from '@fortawesome/free-solid-svg-icons';
+import { faFaceSurprise } from '@fortawesome/free-regular-svg-icons';
 
 export const loadIcons = () => {
-  library.add(faServer);
+  library.add(faServer, faFaceSurprise);
 };
