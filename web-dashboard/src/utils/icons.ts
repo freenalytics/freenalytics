3ef849057d870b9706f41d2b93893dd8392551ce
moonstar-x <christian.lopez99@outlook.com>
2022-10-12 02:18:59 -0500

    Created a loadIcons util for FontAwesome.

---
+++ web-dashboard/src/utils/icons.ts
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faServer } from '@fortawesome/free-solid-svg-icons';
+
+export const loadIcons = () => {
+  library.add(faServer);
+};
