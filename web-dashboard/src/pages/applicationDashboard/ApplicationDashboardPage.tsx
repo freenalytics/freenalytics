478747a3e39d97dc5d599bbac8549e4b80e9cd79
moonstar-x <christian.lopez99@outlook.com>
2022-10-14 00:15:03 -0500

    Application is now fetched in ApplicationDashboardPage.

--- web-dashboard/src/pages/applicationDashboard/ApplicationDashboardPage.tsx
+++ web-dashboard/src/pages/applicationDashboard/ApplicationDashboardPage.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import { Heading } from 'react-bulma-components';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../components/common/loading';
+import RequestErrorMessageFullPage from '../../components/common/requestErrorMessageFullPage';
+import PageWrapper from '../../components/common/pageWrapper';
 import useTitle from '../../hooks/title';
+import useApi from '../../hooks/api';
 
 const ApplicationDashboardPage: React.FC = () => {
-  useTitle('pages.application.dashboard.title', { app: 'My App' });
   const { domain } = useParams();
+  const { client } = useApi();
+  const request = client.application.getApplicationByDomain(domain!);
+  const { isLoading, error, data: application } = useQuery(request.key, request.fn);
+  useTitle('pages.application.dashboard.title', { app: application?.name ?? 'Loading...' });
+
+  if (isLoading) {
+    return (
+      <Loading />
+    );
+  }
+
+  if (error) {
+    return (
+      <RequestErrorMessageFullPage error={error as Error} />
+    );
+  }
 
   return (
-    <div>
-      DASHBOARD - {domain}
-    </div>
+    <PageWrapper>
+      <Heading>
+        {application!.name}
+      </Heading>
+    </PageWrapper>
   );
 };
 
