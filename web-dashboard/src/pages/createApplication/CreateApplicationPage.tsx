6b0620865cf42808a0db9ef388b7e50e686825ae
moonstar-x <christian.lopez99@outlook.com>
2022-10-13 20:42:56 -0500

    Added CreateInstructions to CreateApplicationPage.

--- web-dashboard/src/pages/createApplication/CreateApplicationPage.tsx
+++ web-dashboard/src/pages/createApplication/CreateApplicationPage.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Heading } from 'react-bulma-components';
+import PageWrapper from '../../components/common/pageWrapper';
+import CreateInstructions from '../../components/pageComponents/createApplication/createInstructions';
 import useTitle from '../../hooks/title';
+import useLocale from '../../hooks/locale';
 
 const CreateApplicationPage: React.FC = () => {
   useTitle('pages.create_application.title');
+  const { t } = useLocale();
 
   return (
-    <div>
-      CREATE NEW
-    </div>
+    <PageWrapper>
+      <Heading>
+        {t('pages.create_application.title')}
+      </Heading>
+      <CreateInstructions />
+    </PageWrapper>
   );
 };
 
