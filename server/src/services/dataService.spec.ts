69e2c5c36fd567b53f61089f2358aad89ba47950
moonstar-x <christian.lopez99@outlook.com>
2022-10-22 20:50:16 -0500

    Created getApplicationSchema service.

--- server/src/services/dataService.spec.ts
+++ server/src/services/dataService.spec.ts
@@ -1,8 +1,47 @@
-import { createDataForApplication } from './dataService';
+import { createDataForApplication, getApplicationSchema } from './dataService';
+import { promisify } from 'util';
 const mockingoose = require('mockingoose');
 import Data from '../models/data';
+import Application from '../models/application';
+import redisClient from '../redis/client';
 
-const data1 = {
+jest.mock('redis', () => jest.requireActual('redis-mock'));
+Object.defineProperty(redisClient, 'exists', {
+  value: promisify(redisClient.exists.bind(redisClient))
+});
+Object.defineProperty(redisClient, 'set', {
+  value: promisify(redisClient.set.bind(redisClient))
+});
+Object.defineProperty(redisClient, 'get', {
+  value: promisify(redisClient.get.bind(redisClient))
+});
+Object.defineProperty(redisClient, 'del', {
+  value: promisify(redisClient.del.bind(redisClient))
+});
+Object.defineProperty(redisClient, 'keys', {
+  value: promisify(redisClient.keys.bind(redisClient))
+});
+
+const schema = {
+  type: 'object',
+  properties: {
+    key: {
+      type: 'value'
+    }
+  }
+};
+const app = {
+  name: 'app',
+  owner: 'moon',
+  domain: 'FD-123',
+  type: 'web',
+  template: {
+    raw_schema: '',
+    schema
+  },
+  connectors: []
+};
+const data = {
   domain: 'FD-123',
   payload: {
     key: 'value'
@@ -10,9 +49,50 @@ const data1 = {
 };
 
 describe('Services: DataService', () => {
+  describe('getApplicationSchema()', () => {
+    beforeAll(() => {
+      mockingoose(Application).toReturn((query: any) => {
+        return query.getQuery().domain === app.domain ? app : null;
+      }, 'findOne');
+    });
+
+    beforeEach(async () => {
+      const keys = await redisClient.keys('*');
+      await redisClient.del(keys);
+    });
+
+    afterAll(() => {
+      mockingoose(Application).reset('findOne');
+    });
+
+    it('should resolve the schema cached in redis.', async () => {
+      const cachedSchema = { key: 'value' };
+      await redisClient.set('FD-123:schema', JSON.stringify(cachedSchema));
+
+      const result = await getApplicationSchema('FD-123');
+      expect(result).toMatchObject(cachedSchema);
+    });
+
+    it('should reject if the domain does not resolve to an application.', async () => {
+      await expect(getApplicationSchema('not_found')).rejects.toThrow();
+    });
+
+    it('should update the cache with the schema when retrieved from the database.', async () => {
+      await getApplicationSchema('FD-123');
+      const cachedSchema = await redisClient.get('FD-123:schema') as string;
+
+      expect(JSON.parse(cachedSchema)).toMatchObject(schema);
+    });
+
+    it('should resolve the schema stored in the database.', async () => {
+      const result = await getApplicationSchema('FD-123');
+      expect(result).toMatchObject(schema);
+    });
+  });
+
   describe('createDataForApplication()', () => {
     beforeAll(() => {
-      mockingoose(Data).toReturn(data1, 'save');
+      mockingoose(Data).toReturn(data, 'save');
     });
 
     afterAll(() => {
--- server/src/services/dataService.ts
+++ server/src/services/dataService.ts
@@ -1,4 +1,26 @@
+import redisClient from '../redis/client';
 import Data, { DataModel } from '../models/data';
+import Application from '../models/application';
+import { ResourceNotFoundError } from '../errors/http';
+
+export const getApplicationSchema = async (domain: string): Promise<object> => {
+  const key = `${domain}:schema`;
+  const cacheHit = await redisClient.exists(key);
+
+  if (cacheHit) {
+    const cachedSchema = await redisClient.get(key);
+    return JSON.parse(cachedSchema!);
+  }
+
+  const application = await Application.findOne({ domain }).exec();
+
+  if (!application) {
+    throw new ResourceNotFoundError(`Application ${domain} was not found.`);
+  }
+
+  await redisClient.set(key, JSON.stringify(application.template.schema));
+  return application.template.schema;
+};
 
 export const createDataForApplication = async (domain: string, validData: object): Promise<DataModel> => {
   return await new Data({ domain, payload: validData }).save();
