9f144a42d7cb823d655ab88d05ecf84a5ca0d0d3
moonstar-x <christian.lopez99@outlook.com>
2022-10-23 03:33:12 -0500

    Created get route for data.

--- server/src/controllers/dataController.spec.ts
+++ server/src/controllers/dataController.spec.ts
@@ -1,8 +1,9 @@
-import { create } from './dataController';
+import { create, get } from './dataController';
 import { Request, Response } from 'express';
 import { ResponseMock } from '../../__mocks__/http_mocks';
 import * as dataService from '../services/dataService';
-import { SchemaValidationError } from '../errors/http';
+import { BadRequestError, SchemaValidationError } from '../errors/http';
+import { PAGINATION_DEFAULT_LIMIT, PAGINATION_DEFAULT_START } from '../constants/pagination';
 
 const mockedData = {
   domain: 'FD-123',
@@ -41,7 +42,7 @@ describe('Controllers: DataController', () => {
       createDataForApplicationSpy.mockClear();
     });
 
-    it('should call next if the data payload is invalid.', async () => {
+    it('should call next with a SchemaValidationError if the data payload is invalid.', async () => {
       const req = {
         params: { domain: 'FD-123' },
         body: { key: 123 }
@@ -72,6 +73,74 @@ describe('Controllers: DataController', () => {
 
       expect(res.send).toHaveBeenCalledTimes(1);
       expect((res.send as jest.Mock).mock.calls[0][0].data).toMatchObject(mockedData);
+    });
+  });
+
+  describe('get()', () => {
+    const getDataForApplicationSpy = jest.spyOn(dataService as any, 'getDataForApplication')
+      .mockResolvedValue([mockedData]);
+
+    beforeEach(() => {
+      getDataForApplicationSpy.mockClear();
+    });
+
+    it('should call next with a BadRequestError if the start query parameter is invalid.', async () => {
+      const req = {
+        params: { domain: 'FD-123' },
+        query: { start: 'what' }
+      } as unknown as Request;
+      await get(req, res, nextMock);
+
+      expect(nextMock).toHaveBeenCalledTimes(1);
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(BadRequestError);
+    });
+
+    it('should call next with a BadRequestError if the limit query parameter is invalid.', async () => {
+      const req = {
+        params: { domain: 'FD-123' },
+        query: { limit: 'what' }
+      } as unknown as Request;
+      await get(req, res, nextMock);
+
+      expect(nextMock).toHaveBeenCalledTimes(1);
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(BadRequestError);
+    });
+
+    it('should get the data with the default start and limit if not specified.', async () => {
+      const req = {
+        params: { domain: 'FD-123' },
+        query: {}
+      } as unknown as Request;
+      await get(req, res, nextMock);
+
+      expect(getDataForApplicationSpy).toHaveBeenCalledWith('FD-123', {
+        start: PAGINATION_DEFAULT_START,
+        limit: PAGINATION_DEFAULT_LIMIT
+      });
+    });
+
+    it('should get the data with the provided start and limit values.', async () => {
+      const req = {
+        params: { domain: 'FD-123' },
+        query: { start: 1, limit: 3 }
+      } as unknown as Request;
+      await get(req, res, nextMock);
+
+      expect(getDataForApplicationSpy).toHaveBeenCalledWith('FD-123', {
+        start: 1,
+        limit: 3
+      });
+    });
+
+    it('should respond with the data.', async () => {
+      const req = {
+        params: { domain: 'FD-123' },
+        query: { start: 1, limit: 3 }
+      } as unknown as Request;
+      await get(req, res, nextMock);
+
+      expect(res.send).toHaveBeenCalledTimes(1);
+      expect((res.send as jest.Mock).mock.calls[0][0].data).toMatchObject([mockedData]);
     });
   });
 });
