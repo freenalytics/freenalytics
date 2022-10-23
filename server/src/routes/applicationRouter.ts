6bf54b6f1c1abba0d6fe3928fd586d3d3c8e6dbd
moonstar-x <christian.lopez99@outlook.com>
2022-10-23 01:28:40 -0500

    Added data post route.

--- server/src/routes/applicationRouter.ts
+++ server/src/routes/applicationRouter.ts
@@ -3,6 +3,7 @@ import { verifyUser } from '../middleware/auth';
 import { onlySupportedMethods } from '../middleware/routes';
 import { jsonBodyRequired } from '../middleware/controller';
 import * as applicationController from '../controllers/applicationController';
+import * as dataController from '../controllers/dataController';
 
 const router = express.Router();
 
@@ -16,5 +17,9 @@ router.route('/:domain')
   .patch(verifyUser, jsonBodyRequired, applicationController.updateByDomain)
   .delete(verifyUser, applicationController.deleteByDomain)
   .all(onlySupportedMethods('GET', 'PATCH', 'DELETE'));
+
+router.route('/:domain/data')
+  .post(jsonBodyRequired, dataController.create)
+  .all(onlySupportedMethods('POST'));
 
 export default router;
