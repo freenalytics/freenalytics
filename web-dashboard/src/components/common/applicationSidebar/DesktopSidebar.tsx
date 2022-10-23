3efaa12440e27e98ac34448f475c6bd8c928f69e
moonstar-x <christian.lopez99@outlook.com>
2022-10-23 04:13:11 -0500

    Moved SidebarItems to another component.

--- web-dashboard/src/components/common/applicationSidebar/DesktopSidebar.tsx
+++ web-dashboard/src/components/common/applicationSidebar/DesktopSidebar.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import { Menu } from 'react-bulma-components';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import useLocale from '../../../hooks/locale';
-import { DYNAMIC_PROTECTED_ROUTES } from '../../../constants/routes';
+import SidebarItems from './SidebarItems';
 import { SidebarProps } from './types';
 
 const DesktopSidebar: React.FC<SidebarProps> = ({ active, domain, children }) => {
-  const { t } = useLocale();
-
   return (
     <div className="application-sidebar">
       <Menu>
         <Menu.List>
-          <Menu.List.Item renderAs={Link} to={DYNAMIC_PROTECTED_ROUTES.applicationDashboard(domain)} active={active === 'dashboard'}>
-            <FontAwesomeIcon className="svg-icon" icon="house" />
-            {t('common.application_sidebar.items.dashboard.text')}
-          </Menu.List.Item>
-
-          <Menu.List.Item renderAs={Link} to={DYNAMIC_PROTECTED_ROUTES.applicationInformation(domain)} active={active === 'information'}>
-            <FontAwesomeIcon className="svg-icon" icon="circle-info" />
-            {t('common.application_sidebar.items.information.text')}
-          </Menu.List.Item>
-
-          <Menu.List.Item renderAs={Link} to={DYNAMIC_PROTECTED_ROUTES.applicationSettings(domain)} active={active === 'settings'}>
-            <FontAwesomeIcon className="svg-icon" icon="wrench" />
-            {t('common.application_sidebar.items.settings.text')}
-          </Menu.List.Item>
+          <SidebarItems active={active} domain={domain} />
         </Menu.List>
       </Menu>
 
--- web-dashboard/src/components/common/applicationSidebar/MobileSidebar.tsx
+++ web-dashboard/src/components/common/applicationSidebar/MobileSidebar.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import { Menu } from 'react-bulma-components';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import useLocale from '../../../hooks/locale';
-import { DYNAMIC_PROTECTED_ROUTES } from '../../../constants/routes';
+import SidebarItems from './SidebarItems';
 import { SidebarProps } from './types';
 
 const MobileSidebar: React.FC<SidebarProps> = ({ active, domain, children }) => {
   const [open, setOpen] = useState<boolean>(false);
-  const { t } = useLocale();
 
   const handleToggle = () => {
     setOpen(!open);
@@ -22,20 +19,7 @@ const MobileSidebar: React.FC<SidebarProps> = ({ active, domain, children }) => 
             <FontAwesomeIcon size="2x" icon="arrow-left" />
           </Menu.List.Item>
 
-          <Menu.List.Item renderAs={Link} to={DYNAMIC_PROTECTED_ROUTES.applicationDashboard(domain)} active={active === 'dashboard'}>
-            <FontAwesomeIcon className="svg-icon" icon="house" />
-            {t('common.application_sidebar.items.dashboard.text')}
-          </Menu.List.Item>
-
-          <Menu.List.Item renderAs={Link} to={DYNAMIC_PROTECTED_ROUTES.applicationInformation(domain)} active={active === 'information'}>
-            <FontAwesomeIcon className="svg-icon" icon="circle-info" />
-            {t('common.application_sidebar.items.information.text')}
-          </Menu.List.Item>
-
-          <Menu.List.Item renderAs={Link} to={DYNAMIC_PROTECTED_ROUTES.applicationSettings(domain)} active={active === 'settings'}>
-            <FontAwesomeIcon className="svg-icon" icon="wrench" />
-            {t('common.application_sidebar.items.settings.text')}
-          </Menu.List.Item>
+          <SidebarItems active={active} domain={domain} />
         </Menu.List>
       </Menu>
 
