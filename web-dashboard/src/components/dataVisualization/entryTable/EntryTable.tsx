82687914faad88a701165466bb469d23c663bbc5
moonstar-x <christian.lopez99@outlook.com>
2022-10-23 21:47:52 -0500

    EntryTable page update now changes query param too.

--- web-dashboard/src/components/dataVisualization/entryTable/EntryTable.tsx
+++ web-dashboard/src/components/dataVisualization/entryTable/EntryTable.tsx
@@ -4,8 +4,11 @@ import { useQuery } from '@tanstack/react-query';
 import Loading from '../../common/loading';
 import RequestErrorMessage from '../../common/requestErrorMessage';
 import EntryTableView from './EntryTableView';
-import useApi from '../../../hooks/api';
 import PageSelector from '../../common/pageSelector';
+import useApi from '../../../hooks/api';
+import useQueryParams from '../../../hooks/queryParams';
+
+const QUERY_PARAM_KEY = 'table_start';
 
 interface Props {
   domain: string
@@ -13,13 +16,15 @@ interface Props {
 }
 
 const EntryTable: React.FC<Props> = ({ domain, schema }) => {
-  const [start, setStart] = useState<number>(0);
+  const { queryParams, setQueryParam } = useQueryParams();
+  const [start, setStart] = useState<number>(parseInt(queryParams.get(QUERY_PARAM_KEY) ?? '0', 10));
   const { client } = useApi();
   const request = client.application.getApplicationDataByDomain(domain, { start, limit: 10 });
   const { isLoading, error, data } = useQuery(request.key, request.fn);
 
   const handlePageChange = (pointer: number) => {
     setStart(pointer);
+    setQueryParam(QUERY_PARAM_KEY, pointer.toString());
   };
 
   if (isLoading) {
