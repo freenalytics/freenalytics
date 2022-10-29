4878fd16e9edd521a3ac881013f7e19ef6ab5c3a
moonstar-x <christian.lopez99@outlook.com>
2022-10-29 01:30:41 -0500

    Added owner parameter to data services.

--- server/src/services/dataService.spec.ts
+++ server/src/services/dataService.spec.ts
@@ -1,4 +1,4 @@
-import { createDataForApplication, getApplicationSchema, getDataForApplication, getDataForApplicationAsCsv } from './dataService';
+import { createDataForApplication, getApplicationSchema, getDataForApplicationForUser, getDataForApplicationForUserAsCsv } from './dataService';
 import { promisify } from 'util';
 const mockingoose = require('mockingoose');
 import Data from '../models/data';
@@ -65,20 +65,20 @@ const data3 = {
 };
 
 describe('Services: DataService', () => {
-  describe('getApplicationSchema()', () => {
-    beforeAll(() => {
-      mockingoose(Application).toReturn((query: any) => {
-        return query.getQuery().domain === app.domain ? app : null;
-      }, 'findOne');
-    });
+  beforeAll(() => {
+    mockingoose(Application).toReturn((query: any) => {
+      return query.getQuery().domain === app.domain && (query.getQuery().owner ?? app.owner) === app.owner ? app : null;
+    }, 'findOne');
+  });
 
+  afterAll(() => {
+    mockingoose(Application).reset('findOne');
+  });
+
+  describe('getApplicationSchema()', () => {
     beforeEach(async () => {
       const keys = await redisClient.keys('*');
       await redisClient.del(keys);
-    });
-
-    afterAll(() => {
-      mockingoose(Application).reset('findOne');
     });
 
     it('should resolve the schema cached in redis.', async () => {
@@ -123,7 +123,7 @@ describe('Services: DataService', () => {
     });
   });
 
-  describe('getDataForApplication()', () => {
+  describe('getDataForApplicationForUser()', () => {
     beforeEach(() => {
       mockingoose(Data).toReturn([data1, data2, data3], 'find');
       mockingoose(Data).toReturn(3, 'countDocuments');
@@ -134,21 +134,29 @@ describe('Services: DataService', () => {
       mockingoose(Data).reset('countDocuments');
     });
 
+    it('should reject ResourceNotFoundError if application is not found.', async () => {
+      await expect(getDataForApplicationForUser('FD-12333', 'moon', { start: 5, limit: 1 })).rejects.toThrow(ResourceNotFoundError);
+    });
+
+    it('should reject ResourceNotFoundError if owner is not application owner is not found.', async () => {
+      await expect(getDataForApplicationForUser('FD-123', 'moonstar', { start: 5, limit: 1 })).rejects.toThrow(ResourceNotFoundError);
+    });
+
     it('should reject ResourceNotFoundError if the start index is out of bounds.', async () => {
-      await expect(getDataForApplication('FD-123', { start: 5, limit: 1 })).rejects.toThrow(ResourceNotFoundError);
+      await expect(getDataForApplicationForUser('FD-123', 'moon', { start: 5, limit: 1 })).rejects.toThrow(ResourceNotFoundError);
     });
 
     it('should resolve an empty array if the no data entries exist for the given application.', async () => {
       mockingoose(Data).toReturn([], 'find');
 
-      const result = await getDataForApplication('FD-123', { start: 0, limit: 1 });
+      const result = await getDataForApplicationForUser('FD-123', 'moon', { start: 0, limit: 1 });
       expect(result.result).toHaveLength(0);
     });
 
     it('should resolve the adequate data.', async () => {
       mockingoose(Data).toReturn([data1], 'find');
 
-      const result = await getDataForApplication('FD-123', { start: 0, limit: 1 });
+      const result = await getDataForApplicationForUser('FD-123', 'moon', { start: 0, limit: 1 });
       const expected = {
         result: [data1],
         pagination: {
@@ -165,7 +173,7 @@ describe('Services: DataService', () => {
     });
   });
 
-  describe('getDataForApplicationAsCsv()', () => {
+  describe('getDataForApplicationForUserAsCsv()', () => {
     beforeEach(() => {
       mockingoose(Data).toReturn([data1], 'find');
     });
@@ -174,8 +182,16 @@ describe('Services: DataService', () => {
       mockingoose(Data).reset('find');
     });
 
+    it('should reject ResourceNotFoundError if application is not found.', async () => {
+      await expect(getDataForApplicationForUserAsCsv('FD-12333', 'moon')).rejects.toThrow(ResourceNotFoundError);
+    });
+
+    it('should reject ResourceNotFoundError if owner is not application owner is not found.', async () => {
+      await expect(getDataForApplicationForUserAsCsv('FD-123', 'moonstar')).rejects.toThrow(ResourceNotFoundError);
+    });
+
     it('should resolve the csv data.', async () => {
-      const data = await getDataForApplicationAsCsv('FD-123');
+      const data = await getDataForApplicationForUserAsCsv('FD-123', 'moon');
       const expected = '"createdAt","key"\n"2022-10-14T04:24:22.951Z","value1"';
 
       expect(data).toBe(expected);
