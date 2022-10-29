af910f0357e764be4aa9a753669a401a137afabd
moonstar-x <christian.lopez99@outlook.com>
2022-10-29 02:21:51 -0500

    Locale t function is now cached.

--- web-dashboard/src/hooks/locale.tsx
+++ web-dashboard/src/hooks/locale.tsx
@@ -1,14 +1,15 @@
-import { DEFAULT_LOCALE, isLocaleSupported, translate, TranslateValuesForReact, LocalizedTranslateFunction, MessageKey, ValidLocale } from '../i18n';
+import { useCallback } from 'react';
+import { DEFAULT_LOCALE, isLocaleSupported, translate, LocalizedTranslateFunction } from '../i18n';
 
 const useLocale = () => {
-  const t: LocalizedTranslateFunction = (key: MessageKey, values: TranslateValuesForReact = {}): string => {
+  const t: LocalizedTranslateFunction = useCallback((key, values = {}): string => {
     return translate(DEFAULT_LOCALE, key, values);
-  };
+  }, [DEFAULT_LOCALE]);
 
   return {
     t,
     isLocaleSupported,
-    currentLocale: DEFAULT_LOCALE as ValidLocale
+    currentLocale: DEFAULT_LOCALE
   };
 };
 
