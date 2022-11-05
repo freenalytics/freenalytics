64d7505347e50e5fca86ddb0039f3f03874deff8
moonstar-x <christian.lopez99@outlook.com>
2022-11-04 21:31:59 -0500

    Created a getPathWithTypeForSchema util.

--- web-dashboard/src/utils/schema.ts
+++ web-dashboard/src/utils/schema.ts
@@ -7,3 +7,31 @@ export const getPathForSchema = (schema: any): string[] => {
           .map((p: string) => `${key}.${p}`));
     }, []);
 };
+
+export const getPathWithTypeForSchema = (schema: any): Record<string, string | null> => {
+  return Object.keys(schema.properties)
+    .reduce((acc: Record<string, string | null>, key: string) => {
+      const property = schema.properties[key];
+      const type = property?.type !== 'array' ? property?.type : `${property?.items?.type}[]`;
+
+      if (type === 'object') {
+        const recursiveResult = Object.entries(getPathWithTypeForSchema(schema.properties[key]))
+          .reduce((acc, [p, type]) => {
+            return {
+              ...acc,
+              [`${key}.${p}`]: type
+            };
+          }, {});
+
+        return {
+          ...acc,
+          ...recursiveResult
+        };
+      }
+
+      return {
+        ...acc,
+        [key]: type ?? null
+      };
+    }, {});
+};
