90d355c9bdf3f5140afe76e9819408d8fd83db77
moonstar-x <christian.lopez99@outlook.com>
2022-11-04 22:24:08 -0500

    DataVisualizer renders the correct visualizer based on type.

--- web-dashboard/src/components/dataVisualization/dataVisualizer/DataVisualizer.tsx
+++ web-dashboard/src/components/dataVisualization/dataVisualizer/DataVisualizer.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import StringVisualizer from '../stringVisualizer';
+import StringArrayVisualizer from '../stringArrayVisualizer';
+import NumberVisualizer from '../numberVisualizer';
+import NumberArrayVisualizer from '../numberArrayVisualizer';
+import ObjectArrayVisualizer from '../objectArrayVisualizer';
+import BooleanVisualizer from '../booleanVisualizer';
+import BooleanArrayVisualizer from '../booleanArrayVisualizer';
+import UnsupportedVisualizer from '../unsupportedVisualizer';
 import { SchemaDataType } from '../../../utils/schema';
 
 interface Props {
@@ -7,14 +15,52 @@ interface Props {
   data: object[]
 }
 
+// @ts-ignore
 const DataVisualizer: React.FC<Props> = ({ path, type, data }) => {
+  if (type === 'string') {
+    return (
+      <StringVisualizer />
+    );
+  }
+
+  if (type === 'string[]') {
+    return (
+      <StringArrayVisualizer />
+    );
+  }
+
+  if (type === 'number' || type === 'integer') {
+    return (
+      <NumberVisualizer />
+    );
+  }
+
+  if (type === 'number[]' || type === 'integer[]') {
+    return (
+      <NumberArrayVisualizer />
+    );
+  }
+
+  if (type === 'object[]') {
+    return (
+      <ObjectArrayVisualizer />
+    );
+  }
+
+  if (type === 'boolean') {
+    return (
+      <BooleanVisualizer />
+    );
+  }
+
+  if (type === 'boolean[]') {
+    return (
+      <BooleanArrayVisualizer />
+    );
+  }
+
   return (
-    <div>
-      {type} -- {path}
-      <p style={{ display: 'none' }}>
-        {JSON.stringify(data)}
-      </p>
-    </div>
+    <UnsupportedVisualizer />
   );
 };
 
