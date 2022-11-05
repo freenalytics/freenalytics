d261ab40a2b38732aa433550b1acfa299222b81a
moonstar-x <christian.lopez99@outlook.com>
2022-11-04 20:07:40 -0500

    DataDashboard now fetches data.

--- web-dashboard/src/components/pageComponents/applicationDashboard/dataDashboard/DataDashboard.tsx
+++ web-dashboard/src/components/pageComponents/applicationDashboard/dataDashboard/DataDashboard.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../../common/loading';
+import RequestErrorMessage from '../../../common/requestErrorMessage';
+import useApi from '../../../../hooks/api';
 
 interface Props {
   domain: string
 }
 
 const DataDashboard: React.FC<Props> = ({ domain }) => {
+  const { client } = useApi();
+  const request = client.application.getApplicationDataByDomain(domain, { start: 0, limit: 50 });
+  const { isLoading, error, data } = useQuery(request.key, request.fn);
+
+  if (isLoading) {
+    return (
+      <Loading />
+    );
+  }
+
+  if (error) {
+    return (
+      <RequestErrorMessage error={error as Error} />
+    );
+  }
+
   return (
     <div>
-      {domain}
+      {JSON.stringify(data)}
     </div>
   );
 };
